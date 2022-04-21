b17042bb7d2a6284df5197d8ac79132e760acd2b
James Bury <james.bury@arcticwolf.com>
2022-04-21 15:10:16 -0400

    Fixed streaming job

--- spark/fake-streamer/src/main/scala/SparkClient.scala
+++ spark/fake-streamer/src/main/scala/SparkClient.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.functions.{from_json, sum, window}
 import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
 
 import java.sql.Timestamp
@@ -38,8 +38,10 @@ object SparkClient extends App {
   // Connect to Apache Spark
   val spark:SparkSession = SparkSession.builder()
     .master("local[3]")
-    .appName("SparkClient")
+    .appName("SparkClientV3")
     .getOrCreate()
+
+  spark.conf.set("spark.sql.streaming.metricsEnabled", "true")
 
   import spark.implicits._
   spark.sparkContext.setLogLevel("ERROR")
@@ -49,6 +51,7 @@ object SparkClient extends App {
     .format("socket")
     .option("host", "localhost")
     .option("port", 9999)
+    .option("includeTimestamp", true)
     .load()
 
   // Parse text stream to JSON
@@ -63,6 +66,31 @@ object SparkClient extends App {
       $"source.remote.ip".as("remote_ip"),
       $"source.bytes.recv".as("bytes_recv"),
       $"source.bytes.sent".as("bytes_sent"))
+    .withWatermark("time", "5 seconds")
+
+  val groupedPcrDF = pcrDF
+    .groupBy(window($"time", "30 minutes"), $"device_ip")
+    .agg(
+      (sum($"bytes_sent") - sum($"bytes_recv")) / (sum($"bytes_sent") + sum($"bytes_recv")) as "pcr",
+    )
+
+  val pcrQuery: Unit = groupedPcrDF.writeStream
+    .option("truncate", "false")
+    .format("console")
+    .outputMode("update")
+    .start()
+    .awaitTermination()
+
+  // Print PCR DataFrame
+  /*
+  val pcrQuery: Unit = pcrDF.writeStream
+    .option("truncate", "false")
+    .format("console")
+    .outputMode("append")
+    .start()
+    .awaitTermination()
+  */
+
 
   // Print relevant schema
   /*
@@ -85,6 +113,21 @@ object SparkClient extends App {
   val pcrQuery: Unit = pcrDF.writeStream
     .format("console")
     .outputMode("append")
+    .start()
+    .awaitTermination(10000)
+  */
+
+  // Group by Device IP
+  /*
+  val deviceIpGroupsStream = pcrDF.groupBy(
+    window($"time", "30 minutes", "5 minute"),
+    $"device_ip"
+  ).count()
+
+  val query: Unit = deviceIpGroupsStream.writeStream
+    .queryName("deviceIpGroups")
+    .outputMode("append")
+    .format("console")
     .start()
     .awaitTermination(10000)
   */
